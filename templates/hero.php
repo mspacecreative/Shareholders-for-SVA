a390b20a13805a8005707a6ed3e17056c9fe7372
mspacecreative <matt@mspacecreative.com>
2023-04-02 17:56:54 -0300

    header styles and other CSS tweaks

--- templates/hero.php
+++ templates/hero.php
@@ -1,9 +1,26 @@
 <!-- hero -->
 <section class="hero section light">
 	<div class="inner_container section" style="text-align: center;">
-<h1>Vote for New Voices and Shareholder Representation on the Sernova Board</h1>
-<p style="max-width: 750px; margin-right: auto; margin-left: auto;">Sernova’s Board needs new voices. We are nominating two highly qualified individuals to bring independent shareholder representation to the Board.</p>
-	<img style="max-width: 900px;" class="top-bottom-padding" src="<?php echo get_template_directory_uri(); ?>/assets/img/backgrounds/RollingVsOthers.svg" />
+		<h1>Vote for New Voices and Shareholder Representation on the Sernova Board</h1>
+		<p style="max-width: 750px; margin-right: auto; margin-left: auto;">Sernova’s Board needs new voices. We are nominating two highly qualified individuals to bring independent shareholder representation to the Board.</p>
+		<div class="row dark-text top-margin-4em gutter_space_2">
+			<div class="col col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="boxed">
+					<h2>Dr. Steven Sangha</h2>
+					<p>
+					Dr. Sangha is an experienced and successful investor, executive and corporate director. He has extensive experience investing with start-ups and early stage companies like Sernova and has focused on biotechnology companies, among other areas. A successful medical entrepreneur in the dentistry field, Dr. Sangha also has experience working with pharma/biotechnology companies undertaking clinical trials and engaging with the necessary regulatory processes.
+					</p>
+				</div>
+			</div>
+			<div class="col col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="boxed">
+					<h2>Bertram von Plettenberg</h2>
+					<p>
+					Mr. von Plettenberg has significant corporate planning, financing and M&A expertise. Mr. von Plettenberg worked closely with Sernova’s leadership in 2020-2021 to develop the corporation’s first comprehensive financial plan and helped Sernova engage with a large U.S. medical-technology corporation regarding a potential partnership.
+					</p>
+				</div>
+			</div>
+		</div>
   </div>
 </section>
 <!-- / hero -->
