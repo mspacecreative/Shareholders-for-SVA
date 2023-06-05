757bc15613860c125d2cb269a185f9c9452f21b3
mspacecreative <matt@mspacecreative.com>
2023-06-05 15:31:39 -0300

    bio loop

--- footer.php
+++ footer.php
@@ -12,6 +12,34 @@
 				<p><?php echo __('&copy '); echo date('Y '); echo __('Carson Proxy Advisors. All rights reserved.'); ?></p>
 			</div>
 		</footer>
+
+		<?php
+		$args = array(
+			'post_type' => 'director',
+			'posts_per_page' => -1
+		);
+		$loop = new WP_Query($args);
+		$count = 0;
+		if ($loop->have_posts()) {
+			echo
+			'
+			<div class="modal-backdrop"></div>
+			<div class="modal" style="display:none;">
+				<div class="modal-inner">';
+				while ($loop->have_posts()) {
+					$loop->the_post();
+					$content = get_the_content($loop->ID);
+					$count = $count + 1
+					echo
+					'<div data-id="bio-' . $count . '" class="bio-container">'
+						. $content . 
+					'</div>';
+				}
+				echo
+				'</div>
+			</div>';
+		} wp_reset_query();
+		?>
 	    
 	   	<?php wp_footer(); ?>
 	</body>
