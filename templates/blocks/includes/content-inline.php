3b3654a45dbf69d7576dc5df4aadee6cf0b9027f
mspacecreative <matt@mspacecreative.com>
2023-06-06 11:19:19 -0300

    Replace gold with bold

--- templates/blocks/includes/content-inline.php
+++ templates/blocks/includes/content-inline.php
@@ -6,7 +6,7 @@ $size = 'medium';
 $content = get_sub_field('content');
 					
 if ( $heading ) {
-	echo '<h3 class="gold">' . $heading . '</h3>';
+	echo '<h3 class="bold">' . $heading . '</h3>';
 }
 if ( $icon && $iconsizing && $content ) {
 	echo '<div class="icon row larger middle-lg middle-md middle-sm middle-xs" style="flex-direction: column"><div class="iconContainer alt">' . wp_get_attachment_image( $icon, $size ) '</div><p class="icon-text">' . $content . '</p></div>';
